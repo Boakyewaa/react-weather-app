8d2763fdfc62403d8b31c2b0e87839e2b0b1281d
Berlie <berl.yiadom@gmail.com>
2021-12-31 16:50:44 +0000

    Edited the app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@
 import './App.css';
 import Weather from "./Weather.js"
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       <div className='container'>
       <h1>Weather</h1>
       <Weather />
       <footer>
-        This project was coded by Berlie and is <a href='https://github.com/Boakyewaa/react-weather-app'target="_blank" >open-sourced on Github</a>
+        This project was coded by Berlie and is <a href='https://github.com/Boakyewaa/react-weather-app' target="_blank" rel='noreferrer' >open-sourced on Github</a>
       </footer>
       </div>
     </div>
   );
 }
 
-export default App;
