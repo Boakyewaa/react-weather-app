7deda10c52a600172813da13d70172bdb9d6bf9d
Berlie <berl.yiadom@gmail.com>
2021-12-31 18:47:56 +0000

    Added the css

--- src/Weather.js
+++ src/Weather.js
@@ -3,16 +3,21 @@ import './Weather.css';
 
 export default function Weather(){
     return(
-        <div className="Weather">
-            <h1> Greater Accra </h1>
+        <div className="Main-weather">
+            <div className="search-button">
+                 <input type="text" placeholder="Search for a location" className="search-input"/>
+            <input type="button" className="btn btn-primary" value="Search"/>
+            </div>
+           <div className="Weather">
+               <h1> Greater Accra </h1>
             <ul>
-                <li>Current Weather</li>
+                <li><strong>CURRENT WEATHER</strong></li>
                 <li>3:49</li>
             </ul>
             <div className="row">
                 <div className="col-3">
                       <img src="/images/icon-4.png" alt="weather-icon" />
-            <strong>29°</strong> C
+            <strong>29°</strong>C
                 </div>
                 <div className="col-3">
                    <ul>
@@ -23,6 +28,8 @@ export default function Weather(){
                 </div>
               
             </div>
+           </div>
+            
              
         </div>
     );
