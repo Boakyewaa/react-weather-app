7c7db14ed23cb50a88125eb4a7f68054795326f9
Berlie <berl.yiadom@gmail.com>
2022-01-09 13:06:32 +0000

    Added the weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import './Weather.css';
 
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props){
     const [ready, setReady] = useState(false);
@@ -47,6 +48,7 @@ export default function Weather(props){
                <div className="Weather">
                <WeatherInfo data={weatherData} /> 
            </div> 
+           <WeatherForecast/>
         </div>
     );
     } else{
